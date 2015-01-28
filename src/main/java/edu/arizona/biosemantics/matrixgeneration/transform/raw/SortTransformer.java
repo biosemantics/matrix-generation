b892d2a8307d90fa14edd9e958687496a2db14c3
thomas.rodenhausen <thomas.rodenhausen@gmail.com>
2015-01-28 16:03:24 -0700

    missing class

---
+++ src/main/java/edu/arizona/biosemantics/matrixgeneration/transform/raw/SortTransformer.java
@@ -0,0 +1,12 @@
+package edu.arizona.biosemantics.matrixgeneration.transform.raw;
+
+import edu.arizona.biosemantics.matrixgeneration.model.raw.Matrix;
+
+public class SortTransformer implements Transformer {
+
+	@Override
+	public void transform(Matrix rawMatrix) {
+		rawMatrix.sortAlphabetically();
+	}
+
+}
