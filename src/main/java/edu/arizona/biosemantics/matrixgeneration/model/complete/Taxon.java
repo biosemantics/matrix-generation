8e4728ed9dcbc81af60354b4a98ee562b43a537f
hongcui <hong1.cui@gmail.com>
2017-07-06 16:14:25 -0700

    updates

--- src/main/java/edu/arizona/biosemantics/matrixgeneration/model/complete/Taxon.java
+++ src/main/java/edu/arizona/biosemantics/matrixgeneration/model/complete/Taxon.java
@@ -10,7 +10,6 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-
 import edu.arizona.biosemantics.common.taxonomy.TaxonIdentification;
 
 public class Taxon implements Serializable {
